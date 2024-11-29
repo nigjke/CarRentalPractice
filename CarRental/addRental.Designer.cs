7f71ec23769ab2953631902576e9cfb1a6301d16
Nikita Poteshnov <nik.poteschnow2017@yandex.ru>
2024-11-29 07:33:44 +0300

    fix addRental

--- CarRental/addRental.Designer.cs
+++ CarRental/addRental.Designer.cs
@@ -29,114 +29,257 @@ namespace CarRental
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBoxMake = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.comboBoxModel = new System.Windows.Forms.ComboBox();
             this.comboBoxCustomer = new System.Windows.Forms.ComboBox();
             this.dateTimePickerRentalDate = new System.Windows.Forms.DateTimePicker();
             this.dateTimePickerReturnDate = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
             this.textBoxTotalAmount = new System.Windows.Forms.TextBox();
-            this.buttonAddRental = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.comboBoxMake = new System.Windows.Forms.ComboBox();
+            this.textPhone = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // comboBoxMake
+            // label4
             // 
-            this.comboBoxMake.FormattingEnabled = true;
-            this.comboBoxMake.Location = new System.Drawing.Point(49, 47);
-            this.comboBoxMake.Name = "comboBoxMake";
-            this.comboBoxMake.Size = new System.Drawing.Size(121, 21);
-            this.comboBoxMake.TabIndex = 0;
-            this.comboBoxMake.SelectedIndexChanged += new System.EventHandler(this.comboBoxMake_SelectedIndexChanged);
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(10, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(212, 32);
+            this.label4.TabIndex = 172;
+            this.label4.Text = "Дата окончания";
             // 
-            // comboBoxModel
+            // label3
             // 
-            this.comboBoxModel.FormattingEnabled = true;
-            this.comboBoxModel.Location = new System.Drawing.Point(49, 97);
-            this.comboBoxModel.Name = "comboBoxModel";
-            this.comboBoxModel.Size = new System.Drawing.Size(121, 21);
-            this.comboBoxModel.TabIndex = 1;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(10, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 32);
+            this.label3.TabIndex = 170;
+            this.label3.Text = "Телефон";
             // 
-            // comboBoxCustomer
+            // button2
             // 
-            this.comboBoxCustomer.FormattingEnabled = true;
-            this.comboBoxCustomer.Location = new System.Drawing.Point(49, 157);
-            this.comboBoxCustomer.Name = "comboBoxCustomer";
-            this.comboBoxCustomer.Size = new System.Drawing.Size(121, 21);
-            this.comboBoxCustomer.TabIndex = 2;
-            // 
-            // dateTimePickerRentalDate
-            // 
-            this.dateTimePickerRentalDate.Location = new System.Drawing.Point(30, 234);
-            this.dateTimePickerRentalDate.Name = "dateTimePickerRentalDate";
-            this.dateTimePickerRentalDate.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePickerRentalDate.TabIndex = 3;
-            // 
-            // dateTimePickerReturnDate
-            // 
-            this.dateTimePickerReturnDate.Location = new System.Drawing.Point(30, 297);
-            this.dateTimePickerReturnDate.Name = "dateTimePickerReturnDate";
-            this.dateTimePickerReturnDate.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePickerReturnDate.TabIndex = 4;
-            this.dateTimePickerReturnDate.ValueChanged += new System.EventHandler(this.dateTimePickerReturnDate_ValueChanged);
-            // 
-            // textBoxTotalAmount
-            // 
-            this.textBoxTotalAmount.Location = new System.Drawing.Point(49, 354);
-            this.textBoxTotalAmount.Name = "textBoxTotalAmount";
-            this.textBoxTotalAmount.Size = new System.Drawing.Size(100, 20);
-            this.textBoxTotalAmount.TabIndex = 5;
-            // 
-            // buttonAddRental
-            // 
-            this.buttonAddRental.Location = new System.Drawing.Point(49, 408);
-            this.buttonAddRental.Name = "buttonAddRental";
-            this.buttonAddRental.Size = new System.Drawing.Size(75, 20);
-            this.buttonAddRental.TabIndex = 6;
-            this.buttonAddRental.Text = "button1";
-            this.buttonAddRental.UseVisualStyleBackColor = true;
-            this.buttonAddRental.Click += new System.EventHandler(this.buttonAddRental_Click);
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(16, 496);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(451, 59);
+            this.button2.TabIndex = 165;
+            this.button2.Text = "Назад";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(49, 479);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(96)))), ((int)(((byte)(255)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(16, 421);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 20);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Size = new System.Drawing.Size(451, 59);
+            this.button1.TabIndex = 164;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(10, 228);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(170, 32);
+            this.label8.TabIndex = 169;
+            this.label8.Text = "Дата начала";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(10, 126);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(129, 32);
+            this.label7.TabIndex = 168;
+            this.label7.Text = "Фамилия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(10, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 32);
+            this.label2.TabIndex = 167;
+            this.label2.Text = "Модель";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(10, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 32);
+            this.label1.TabIndex = 166;
+            this.label1.Text = "Марка";
+            // 
+            // comboBoxModel
+            // 
+            this.comboBoxModel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxModel.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxModel.FormattingEnabled = true;
+            this.comboBoxModel.Items.AddRange(new object[] {
+            ""});
+            this.comboBoxModel.Location = new System.Drawing.Point(221, 73);
+            this.comboBoxModel.Name = "comboBoxModel";
+            this.comboBoxModel.Size = new System.Drawing.Size(242, 32);
+            this.comboBoxModel.TabIndex = 176;
+            // 
+            // comboBoxCustomer
+            // 
+            this.comboBoxCustomer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCustomer.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxCustomer.FormattingEnabled = true;
+            this.comboBoxCustomer.Items.AddRange(new object[] {
+            ""});
+            this.comboBoxCustomer.Location = new System.Drawing.Point(222, 126);
+            this.comboBoxCustomer.Name = "comboBoxCustomer";
+            this.comboBoxCustomer.Size = new System.Drawing.Size(242, 32);
+            this.comboBoxCustomer.TabIndex = 177;
+            this.comboBoxCustomer.SelectedIndexChanged += new System.EventHandler(this.comboBoxCustomer_SelectedIndexChanged);
+            // 
+            // dateTimePickerRentalDate
+            // 
+            this.dateTimePickerRentalDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePickerRentalDate.Location = new System.Drawing.Point(222, 229);
+            this.dateTimePickerRentalDate.Name = "dateTimePickerRentalDate";
+            this.dateTimePickerRentalDate.Size = new System.Drawing.Size(242, 31);
+            this.dateTimePickerRentalDate.TabIndex = 180;
+            // 
+            // dateTimePickerReturnDate
+            // 
+            this.dateTimePickerReturnDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePickerReturnDate.Location = new System.Drawing.Point(221, 280);
+            this.dateTimePickerReturnDate.Name = "dateTimePickerReturnDate";
+            this.dateTimePickerReturnDate.Size = new System.Drawing.Size(242, 31);
+            this.dateTimePickerReturnDate.TabIndex = 181;
+            this.dateTimePickerReturnDate.ValueChanged += new System.EventHandler(this.dateTimePickerReturnDate_ValueChanged_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(10, 333);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 32);
+            this.label5.TabIndex = 182;
+            this.label5.Text = "Сумма";
+            // 
+            // textBoxTotalAmount
+            // 
+            this.textBoxTotalAmount.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.textBoxTotalAmount.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxTotalAmount.ForeColor = System.Drawing.Color.White;
+            this.textBoxTotalAmount.Location = new System.Drawing.Point(221, 333);
+            this.textBoxTotalAmount.MaxLength = 20;
+            this.textBoxTotalAmount.Name = "textBoxTotalAmount";
+            this.textBoxTotalAmount.ReadOnly = true;
+            this.textBoxTotalAmount.Size = new System.Drawing.Size(242, 32);
+            this.textBoxTotalAmount.TabIndex = 183;
+            // 
+            // comboBoxMake
+            // 
+            this.comboBoxMake.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMake.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxMake.FormattingEnabled = true;
+            this.comboBoxMake.Items.AddRange(new object[] {
+            ""});
+            this.comboBoxMake.Location = new System.Drawing.Point(221, 23);
+            this.comboBoxMake.Name = "comboBoxMake";
+            this.comboBoxMake.Size = new System.Drawing.Size(242, 32);
+            this.comboBoxMake.TabIndex = 184;
+            this.comboBoxMake.SelectedIndexChanged += new System.EventHandler(this.comboBoxMake_SelectedIndexChanged_1);
+            // 
+            // textPhone
+            // 
+            this.textPhone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.textPhone.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textPhone.ForeColor = System.Drawing.Color.White;
+            this.textPhone.Location = new System.Drawing.Point(221, 181);
+            this.textPhone.MaxLength = 20;
+            this.textPhone.Name = "textPhone";
+            this.textPhone.ReadOnly = true;
+            this.textPhone.Size = new System.Drawing.Size(242, 32);
+            this.textPhone.TabIndex = 185;
             // 
             // addRental
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(445, 629);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.buttonAddRental);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(482, 588);
+            this.Controls.Add(this.textPhone);
+            this.Controls.Add(this.comboBoxMake);
             this.Controls.Add(this.textBoxTotalAmount);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.dateTimePickerReturnDate);
             this.Controls.Add(this.dateTimePickerRentalDate);
             this.Controls.Add(this.comboBoxCustomer);
             this.Controls.Add(this.comboBoxModel);
-            this.Controls.Add(this.comboBoxMake);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "addRental";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "addRental";
+            this.Load += new System.EventHandler(this.addRental_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox comboBoxMake;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBoxModel;
         private System.Windows.Forms.ComboBox comboBoxCustomer;
         private System.Windows.Forms.DateTimePicker dateTimePickerRentalDate;
         private System.Windows.Forms.DateTimePicker dateTimePickerReturnDate;
+        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBoxTotalAmount;
-        private System.Windows.Forms.Button buttonAddRental;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBoxMake;
+        private System.Windows.Forms.TextBox textPhone;
     }
 }
--- CarRental/addRental.cs
+++ CarRental/addRental.cs
@@ -34,7 +34,7 @@ namespace CarRental
                 comboBoxMake.DataSource = dataTableMake;
                 comboBoxMake.DisplayMember = "make";
 
-                string queryCustomers = "SELECT customer_id, last_name FROM customers";
+                string queryCustomers = "SELECT customer_id, last_name,phone FROM customers";
                 MySqlDataAdapter adapterCustomers = new MySqlDataAdapter(queryCustomers, connection);
                 DataTable dataTableCustomers = new DataTable();
                 adapterCustomers.Fill(dataTableCustomers);
@@ -42,23 +42,6 @@ namespace CarRental
                 comboBoxCustomer.DisplayMember = "last_name";
                 comboBoxCustomer.ValueMember = "customer_id";
 
-            }
-        }
-
-        private void comboBoxMake_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            string selectedMake = comboBoxMake.Text;
-            string queryModel = "SELECT model FROM cars WHERE make = @make";
-
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-            {
-                MySqlCommand command = new MySqlCommand(queryModel, connection);
-                command.Parameters.AddWithValue("@make", selectedMake);
-                MySqlDataAdapter adapterModel = new MySqlDataAdapter(command);
-                DataTable dataTableModel = new DataTable();
-                adapterModel.Fill(dataTableModel);
-                comboBoxModel.DataSource = dataTableModel;
-                comboBoxModel.DisplayMember = "model";
             }
         }
         private void CalculateTotalAmount()
@@ -101,28 +84,6 @@ namespace CarRental
                 textBoxTotalAmount.Text = totalAmount.ToString("C");
             }
         }
-
-        private void buttonAddRental_Click(object sender, EventArgs e)
-        {
-            string connectionString = db.connect;
-            string query = "INSERT INTO rentals (customer_id, car_id, rental_date, return_date, total_amount) VALUES (@customer_id, @car_id, @rental_date, @return_date, @total_amount)";
-
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-            {
-                MySqlCommand command = new MySqlCommand(query, connection);
-                command.Parameters.AddWithValue("@customer_id", comboBoxCustomer.SelectedValue);
-                command.Parameters.AddWithValue("@car_id", GetCarId(comboBoxMake.Text, comboBoxModel.Text));
-                command.Parameters.AddWithValue("@rental_date", dateTimePickerRentalDate.Value);
-                command.Parameters.AddWithValue("@return_date", dateTimePickerReturnDate.Value);
-                command.Parameters.AddWithValue("@total_amount", decimal.Parse(textBoxTotalAmount.Text, NumberStyles.Currency));
-
-                connection.Open();
-                command.ExecuteNonQuery();
-            }
-
-            this.Close();
-        }
-
         private int GetCarId(string make, string model)
         {
             string connectionString = db.connect;
@@ -137,12 +98,78 @@ namespace CarRental
                 return (int)command.ExecuteScalar();
             }
         }
+        private void comboBoxMake_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            string selectedMake = comboBoxMake.Text;
+            string queryModel = "SELECT model FROM cars WHERE make = @make";
 
-        private void dateTimePickerReturnDate_ValueChanged(object sender, EventArgs e)
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand command = new MySqlCommand(queryModel, connection);
+                command.Parameters.AddWithValue("@make", selectedMake);
+                MySqlDataAdapter adapterModel = new MySqlDataAdapter(command);
+                DataTable dataTableModel = new DataTable();
+                adapterModel.Fill(dataTableModel);
+                comboBoxModel.DataSource = dataTableModel;
+                comboBoxModel.DisplayMember = "model";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBoxModel.Text != "" && comboBoxCustomer.Text != "" && comboBoxMake.Text != "" && dateTimePickerRentalDate.Text != "" && dateTimePickerReturnDate.Text != "" && textBoxTotalAmount.Text != "")
+            {
+                string connectionString = db.connect;
+                string query = "INSERT INTO rentals (customer_id, car_id, rental_date, return_date, total_amount) VALUES (@customer_id, @car_id, @rental_date, @return_date, @total_amount)";
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@customer_id", comboBoxCustomer.SelectedValue);
+                    command.Parameters.AddWithValue("@car_id", GetCarId(comboBoxMake.Text, comboBoxModel.Text));
+                    command.Parameters.AddWithValue("@rental_date", dateTimePickerRentalDate.Value);
+                    command.Parameters.AddWithValue("@return_date", dateTimePickerReturnDate.Value);
+                    command.Parameters.AddWithValue("@total_amount", decimal.Parse(textBoxTotalAmount.Text, NumberStyles.Currency));
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Заполните все поля");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dateTimePickerReturnDate_ValueChanged_1(object sender, EventArgs e)
         {
             CalculateTotalAmount();
         }
+
+        private void addRental_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxCustomer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxCustomer.SelectedIndex != -1)
+            {
+                DataRowView selectedRow = comboBoxCustomer.SelectedItem as DataRowView;
+                if (selectedRow != null)
+                {
+                    textPhone.Text = selectedRow["phone"].ToString();
+                }
+            }
+        }
     }
-       
 }
+   
 
